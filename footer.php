add5e1cb023750b160e5d964aef10bd8fbb740a6
mwhc00 <melvinwonghc@gmail.com>
2021-12-19 10:59:07 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        <div class="footer-copyright text-center py-3">
+            <small><a href="https://crypto2share.com">Crypto2Share</a>. Contact us at hi (at) crypto2share.com</small>
+            <div class="pt-2"></div>
+            <small>Built with &#10084; and &#9728; to spread the goodnews of cryptocurrency. Made in Seoul, South Korea. <a href="https://github.com/melwong/crypto2share"><i class="fab fa-github"></i></a></small>
+        </div>
+    </footer>
